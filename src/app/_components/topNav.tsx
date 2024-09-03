dddf65962c56010213f12f785e367c3bf5c9d3fd
sravanpant <sravan.pant5@gmail.com>
2024-09-03 23:30:58 +0530

    Added Authentication

---
+++ src/app/_components/topNav.tsx
@@ -0,0 +1,20 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import React from "react";
+
+function TopNav() {
+  return (
+    <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
+      <div>Gallery</div>
+      <div>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
+    </nav>
+  );
+}
+
+export default TopNav;
